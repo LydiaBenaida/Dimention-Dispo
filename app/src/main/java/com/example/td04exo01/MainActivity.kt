e2eb55c5ea31704ee65afac1c8d480fcf9be7ef7
LydiaBenaida <gl_benaida@esi.dz>
2020-04-11 18:01:24 +0200

    dimensions et dispositions

--- app/src/main/java/com/example/td04exo01/MainActivity.kt
+++ app/src/main/java/com/example/td04exo01/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.td04exo01
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
     }
 }
